b79a315a0dfbf41dde8a252cff58c825f80dcf48
Priscila Cabral <priscilacabral.simao@gmail.com>
2020-04-21 19:42:39 -0300

    Created all methods

--- src/app.js
+++ src/app.js
@@ -31,15 +31,32 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex === -1){
+    return response.status(400).json({error: 'Repository does not exists '});
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes,
+  }
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  const findRepositoryIndex = repositories.findIndex(repository => repository.id === id);
-  if (findRepositoryIndex > 0){
-    repositories.splice(findRepositoryIndex, 1);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if (repositoryIndex > 0){
+    repositories.splice(repositoryIndex, 1);
   }
   else{
     return response.status(400).json({ error: 'Repository does not exists. '})
@@ -50,7 +67,18 @@ app.delete("/repositories/:id", (request, response) => {
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex === -1){
+    return response.status(400).json({error: 'Repository does not exists '});
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex]);
+
 });
 
 module.exports = app;
